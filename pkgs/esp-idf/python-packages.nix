cae6dab5d18b3f1014dbb84bd74011c317c9c3e2
J <47265397+UserJHansen@users.noreply.github.com>
2025-07-19 18:48:40 +1000

    Check setting format

--- pkgs/esp-idf/python-packages.nix
+++ pkgs/esp-idf/python-packages.nix
@@ -13,7 +13,7 @@ with pythonPackages; rec {
     pname = "idf-component-manager";
     version = "1.3.2";
 
-    pyproject = true;
+    format = "pyproject";
 
     src = fetchFromGitHub {
       owner = "espressif";
